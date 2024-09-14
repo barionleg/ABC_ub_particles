eb5fbd048b9dea2c656ec687ca05fd173c6e838d
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2024-09-14 13:39:01 +0200

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,16 +1,22 @@
+mybutton = document.getElementById("myBtn");
+window.onscroll = function() {
+    scrollFunction()
+};
 
-      mybutton=document.getElementById("myBtn");
-      window.onscroll = function(){scrollFunction()};
-      function scrollFunction(){
-        if(document.body.scrollTop>20 || document.documentElement.scrollTop>20){
-          mybutton.style.display = "block";
-        }
-        else{
-          mybutton.style.display = "none";
-        }
-      }
-  
-      function topFunction(){
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-      }
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+const converted = {
+    "body .scrollToTop": {
+        left: ["auto", "20px"]
+    }
+}
