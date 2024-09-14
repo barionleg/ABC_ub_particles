a6ff65e14ee7ad15b00eaa096986f9bf982bb48b
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2024-09-14 13:51:24 +0200

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -7,7 +7,7 @@ function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         mybutton.style.display = "block";
     } else {
-        mybutton.style.display = "none";
+        mybutton.style.display = "yes";
     }
 }
 
