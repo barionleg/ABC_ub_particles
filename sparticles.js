33cd2afac32216fd4a6b097cd79ea7a90d121441
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2024-12-15 13:40:24 +0100

    Add files via upload

---
+++ sparticles.js
@@ -0,0 +1,102 @@
+    //*𒀭 && 🎶 ibanknatoprad 𝄫 barionleg Aibolem 𝄞 Composar© ❤🎵*//
+   //* avalible shape=image                                       *//
+  //* https://aibolem.github.io/dbrein/FrontEnd/img/ARTPARIS.png *//
+ //* https://aibolem.github.io/dbrein/TAR_PARISი.png            *//
+// https://aibolem.github.io/dbrein/ARg🎨PARIS.png             *//
+
+particlesJS('particles-js', {
+ 'particles': {
+ 'number': {
+ 'value': 69,
+ 'density': {
+ 'enable': true,
+ 'value_area': 800
+ }
+ },
+ 'color': { 'value': '#ff0266',},
+ 'shape': {
+ 'type': 'image',
+ 'stroke': {
+ 'width': 3,
+ 'color': '#000000'
+ },
+ 'edge': { 'nb_sides': 5 },
+ 'image': {
+ 'src': 'https://aibolem.github.io/dbrein/FrontEnd/TAR_PARIS.png',
+ 'width': 13,
+ 'height': 13
+ }
+ },
+ 'opacity': {
+ 'value': 0.9,
+ 'random': true,
+ 'anim': {
+ 'enable': true,
+ 'speed': 3,
+ 'opacity_min': 0.7,
+ 'sync': false
+ }
+ },
+ 'size': {
+ 'value': 13,
+ 'random': true,
+ 'anim': {
+ 'enable': true,
+ 'speed': 5,
+ 'size_min': 10,
+ 'sync': false
+ }
+ },
+ 'line_linked': {
+ 'enable': true,
+ 'distance': 150,
+ 'color': "#03dac6",
+ 'opacity': 0.9,
+ 'width': 3
+ },
+ 'move': {
+ 'enable': true,
+ 'speed': 3,
+ 'direction': 'none',
+ 'random': true,
+ 'straight': false,
+ 'out_mode': 'out',
+ 'bounce': true,
+ 'attract': {
+ 'enable': true,
+ 'rotateX': 600,
+ 'rotateY': 1200
+ }
+ }
+ },
+ 'interactivity': {
+ 'detect_on': 'canvas',
+ 'events': {
+     'onhover': {
+ 'enable': true,
+ 'mode': 'grab'
+ },
+ 'resize': true
+ },
+ 'modes': {
+ 'grab': {
+ 'distance': 140,
+ 'line_linked': { 'opacity': 0.9 }
+ },
+ 'bubble': {
+ 'distance': 300,
+ 'size': 15,
+ 'duration': 2,
+ 'opacity': 0.8,
+ 'speed': 3
+ },
+ 'repulse': {
+ 'distance': 140,
+ 'duration': 0.4
+ },
+ 'push': { 'particles_nb': 4 },
+ 'remove': { 'particles_nb': 2 }
+ }
+ },
+ 'retina_detect': true
+ });
